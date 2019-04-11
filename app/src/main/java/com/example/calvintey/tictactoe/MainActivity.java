d16a1b27c7590dd47bf71260dddb6e4b55d9bf93
calvintts <calvinteytaisoon@gmail.com>
2019-04-10 23:55:15 -0700

    quick fix

--- app/src/main/java/com/example/calvintey/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/calvintey/tictactoe/MainActivity.java
@@ -10,18 +10,23 @@ public class MainActivity extends AppCompatActivity {
     int p1 = 0;
     int[] used={2,2,2,2,2,2,2,2,2};
     int[][] won={{0,1,2},{3,4,5},{6,7,8},{0,4,8},{1,4,7},{2,5,8},{0,3,6},{2,5,8}};
-    public void dropIn(View view){
+    public void dropIn(View view) {
         ImageView counter = (ImageView) view;
-        counter.setTranslationY(-1500);
-        counter.setImageResource((p1==0?R.drawable.cross:R.drawable.circle));
-        counter.animate().translationYBy(1500).rotation(360*5).setDuration(1000);
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
-        used[tappedCounter]=p1;
-        for(int[] position:won){
-            if(used[position[0]]==used[position[1]]&&used[position[1]]==used[position[2]]&&used[position[0]]!=2) {
-                Toast.makeText(this,"Player"+(p1+1)+" has won!",Toast.LENGTH_SHORT).show();
-            }}
-        p1=(p1==0?1:0);
+        boolean gameActive=true;
+        if (used[tappedCounter] == 2&&gameActive) {
+            counter.setTranslationY(-1500);
+            counter.setImageResource((p1 == 0 ? R.drawable.cross : R.drawable.circle));
+            counter.animate().translationYBy(1500).rotation(360 * 5).setDuration(1000);
+            used[tappedCounter] = p1;
+            for (int[] position : won) {
+                if (used[position[0]] == used[position[1]] && used[position[1]] == used[position[2]] && used[position[0]] != 2) {
+                    Toast.makeText(this, "Player" + (p1 + 1) + " has won!", Toast.LENGTH_SHORT).show();
+                    gameActive=false;
+                }
+            }
+            p1 = (p1 == 0 ? 1 : 0);
+        }
     }
 
     @Override
